ce332b0c36a95b2afcc240899a986e95d36a80c1
Santiago Cartagena <scartagena147@gmail.com>
2022-04-12 22:59:43 -0300

    Loader

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,6 +8,7 @@ import {
 import { faDatabase } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
@@ -21,56 +22,59 @@ const About = () => {
   }, [])
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={Array.from('About me')}
-            index={15}
-          />
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          accumsan quam ac est laoreet vulputate. Praesent interdum ac ligula et
-          porta.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          accumsan quam ac est laoreet vulputate. Praesent interdum ac ligula et
-          porta.
-        </p>
-        <p>
-          {' '}
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          accumsan quam ac est laoreet vulputate. Praesent interdum ac ligula et
-          porta.
-        </p>
-      </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={Array.from('About me')}
+              index={15}
+            />
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            accumsan quam ac est laoreet vulputate. Praesent interdum ac ligula
+            et porta.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            accumsan quam ac est laoreet vulputate. Praesent interdum ac ligula
+            et porta.
+          </p>
+          <p>
+            {' '}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            accumsan quam ac est laoreet vulputate. Praesent interdum ac ligula
+            et porta.
+          </p>
+        </div>
 
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faJava} color="#DD0031" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faNodeJs} color="#68A063" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faAndroid} color="#3DDB85" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faDatabase} color="#B3BCC9" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faJava} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faNodeJs} color="#68A063" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faAndroid} color="#3DDB85" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faDatabase} color="#B3BCC9" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="ball-clip-rotate-multiple" />
+    </>
   )
 }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
 import AnimatedLetters from '../AnimatedLetters'
@@ -17,34 +18,37 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            index={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            index={22}
-          />
-        </h1>
-        <h2>Developer / Junior / Hire Me!</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              index={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              index={22}
+            />
+          </h1>
+          <h2>Developer / Junior / Hire Me!</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="ball-clip-rotate-multiple" />
+    </>
   )
 }
 
