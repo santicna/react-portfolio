138b065cf284fcfb61b1a1fc3728422600ac817f
santicna <scartagena147@gmail.com>
2022-04-18 11:02:44 -0300

    Tags cleanup

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,13 +7,11 @@ const Layout = () => {
     <div className="App">
       <TopNavbar />
       <div className="page">
-        <span className="tags top-tags">&lt;body&gt;</span>
-
+        <span className="tags top-tags"></span>
         <Outlet />
         <span className="tags bottom-tags">
-          &lt;/body&gt;
           <br />
-          <span className="bottom-tag-html">&lt;/html&gt;</span>
+          <span className="bottom-tag-html"></span>
         </span>
       </div>
     </div>
